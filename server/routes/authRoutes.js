8302e3be3fa96aac4de3b29172fafdebdbe5d20f
dhilrohith <rohithbj2018@gmail.com>
2025-02-16 21:37:58 +0530

    removed user routes

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -3,23 +3,7 @@ import express from 'express';
 import {authController} from 
 '../contollers/authController.js'
 
-import {authorize, protect} from '../middlewares/authMiddleware.js'
-
 export const authRouter = express.Router();
 
 authRouter.post(`/register`, authController.register);
 authRouter.post(`/login`, authController.login);
-
-// routes for authenticated user
-authRouter.get(`/users/:userId`, 
-    protect, authController.getUser
-);
-authRouter.put(`/users/:userId`, 
-    protect, authController.updateUser
-);
-
-// admin route (protected route)
-authRouter.get(`/users`, 
-    protect, authorize('admin'), 
-    authController.getAllUsers
-);
