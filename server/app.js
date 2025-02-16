8abb0dfd92f4b5922c68d3138a825daae08c2882
dhilrohith <rohithbj2018@gmail.com>
2025-02-16 21:36:26 +0530

    updated the default routes and added error handler

--- server/app.js
+++ server/app.js
@@ -1,9 +1,18 @@
 import express from 'express';
-import { authRouter } from './routes/authRoutes.js';
+
+import { authRouter} from './routes/authRoutes.js';
+
+import {errorHandler} from './middlewares/errorMiddleware.js';
+
+import {userRouter} from './routes/userRoutes.js'
+
 const app = express();
 
 app.use(express.json());
 
 app.use(`/api/v1/auth`, authRouter);
+
+app.use(`/api/v1/users`, userRouter);
+app.use(errorHandler);
 
 export default app;
