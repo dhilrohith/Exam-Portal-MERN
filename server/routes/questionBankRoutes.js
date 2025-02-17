d9324ff26fddd3649ef78f229fbc3660ad276e9f
dhilrohith <rohithbj2018@gmail.com>
2025-02-17 12:14:45 +0530

    questionBank routes updated with middlewares

--- server/routes/questionBankRoutes.js
+++ server/routes/questionBankRoutes.js
@@ -1,9 +1,28 @@
 import express from 'express';
 
+import { authorize, protect } from 
+'../middlewares/authMiddleware.js';
+
+import { questionBankController } from 
+'../contollers/questionBankController.js';
+
 export const questionBankRouter = express.Router();
 
-questionBankRouter.post(`/`);
-questionBankRouter.get(`/`);
-questionBankRouter.get(`/:bankId`);
-questionBankRouter.put(`/:bankId`);
-questionBankRouter.delete(`/:bankId`);
+questionBankRouter.post(`/`,
+    protect, authorize('admin'),
+    questionBankController.createQuestionBank
+);
+questionBankRouter.get(`/`,
+    questionBankController.getAllQuestionBanks
+);
+questionBankRouter.get(`/:bankId`,
+    questionBankController.getQuestionBankById
+);
+questionBankRouter.put(`/:bankId`,
+    protect, authorize('admin'),
+    questionBankController.updateQuestionBank
+);
+questionBankRouter.delete(`/:bankId`,
+    protect, authorize('admin'),
+    questionBankController.deleteQuestionBank
+);
