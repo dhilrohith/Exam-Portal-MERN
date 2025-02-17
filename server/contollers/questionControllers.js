030561ce26fa32996b82b65c3ce0b6faf6fc6b1a
dhilrohith <rohithbj2018@gmail.com>
2025-02-17 15:22:04 +0530

    update question and delete question controllers added

--- server/contollers/questionControllers.js
+++ server/contollers/questionControllers.js
@@ -48,5 +48,93 @@ export const questionController = {
         } catch(error){
             next(error)
         }
+    },
+
+    updateQuestionInBank: async(req, res, next)=>{
+        try{
+            const {bankId, questionId} = req.params;
+
+            const questionBank = await QuestionBank
+            .findById(bankId);
+
+            if(!questionBank){
+                return res.status(404).json({
+                    error: 'Question bank not found'
+                });
+            }
+
+            // find the question by its subdocument id
+            const question = questionBank.questions
+            .id(questionId);
+
+            if(!question){
+                return res.status(404).json({
+                    error: 'Question not found'
+                });
+            }
+
+            // allowed fields for the question update
+            const allowedFields = [
+                'type', 
+                'questionText', 
+                'options', 
+                'correctAnswer', 
+                'topic', 
+                'difficulty', 
+                'examType', 
+                'explanation'
+            ]
+
+            allowedFields.forEach((field)=>{
+                if(req.body[field] !== undefined){
+                    question[field] = req.body[field];
+                }
+            });
+
+            await questionBank.save();
+            res.json({
+                message: 'Question updated successfully',
+                questionBank,
+              });
+
+        } catch(error){
+            next(error);
+        }
+    },
+
+    deleteQuestionInBank: async (req, res, next)=>{
+        try{
+            const {bankId, questionId} = req.params;
+
+            const questionBank = await QuestionBank
+            .findById(bankId);
+
+            if(!questionBank){
+                return res.status(404).json({
+                    error: 'Question bank not found'
+                });
+            }
+
+            // get question by it subdocument id
+            const question = questionBank.questions
+            .id(questionId);
+
+            if(!question){
+                return res.status(404).json({
+                    error: 'Question not found'
+                });
+            }
+
+            question.deleteOne();
+            await questionBank.save();
+            res.json({
+                message: 
+                "Question deleted successfully in question bank",
+                question,
+            });
+
+        } catch(error){
+            next(error)
+        }
     }
 }
