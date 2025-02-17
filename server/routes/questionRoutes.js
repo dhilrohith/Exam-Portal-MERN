cb3f66823efdc17c63a524fd4afd187aeca3a22d
dhilrohith <rohithbj2018@gmail.com>
2025-02-17 14:13:11 +0530

    removed question routes

--- server/routes/questionRoutes.js
+++
@@ -1,7 +0,0 @@
-import express from 'express';
-
-export const questionRouter = express.Router();
-
-questionRouter.post(`/`);
-questionRouter.put(`/:questionId`)
-questionRouter.delete(`/:questionId`);
