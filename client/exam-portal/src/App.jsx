b69824cce32f75613456c5a94eece3b74c76712d
dhilrohith <rohithbj2018@gmail.com>
2025-03-05 23:16:58 +0530

    styles changed

--- client/exam-portal/src/App.jsx
+++ client/exam-portal/src/App.jsx
@@ -24,7 +24,7 @@ function App() {
       <div className="flex flex-col min-h-screen bg-black text-white">
         <Navbar />
 
-        <main className="flex-1">
+        <main className="flex-1 flex flex-col">
           <Routes>
               <Route path="/" element={<Home />} />
               <Route path="/login" element={<Login />} />
